f87c61b72d434b9235bbea338623976bf0192b91
Kat <jirehrecinos@gmail.com>
2023-10-07 22:47:32 -0600

    Ya casi termino...medijo vitor xd

---
+++ Controllers/DevolucionController.cs
@@ -0,0 +1,40 @@
+﻿using DesarrolloWeb.Models;
+using DesarrolloWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesarrolloWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevolucionController : Controller
+    {
+        private readonly IDevolucion DevolucionServices;
+
+        public DevolucionController(IDevolucion devolucionServices)
+        {
+            DevolucionServices = devolucionServices;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Devolucion>>> GetDevolucion()
+        {
+            List<Devolucion> lista = await DevolucionServices.GetDevolucion();
+
+            if (lista == null) return NotFound("no me sale, soy gay");
+
+            return Ok(lista);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Devolucion>>> GetDevolucionxId(int id)
+        {
+            List<Devolucion> lista = await DevolucionServices.GetDevolucionxId(id);
+
+            if (lista == null) return NotFound("no me sale, soy gay");
+
+            return Ok(lista);
+        }
+
+    }
+}
---
+++ DTOs/CierreCajaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DesarrolloWeb.DTOs
+{
+    public class CierreCajaDTO
+    {
+        public int Id_Empleado { get; set; }
+        public decimal Saldo { get; set; }
+        public int Id_Apertura { get; set; }
+
+
+       
+    }
+}
